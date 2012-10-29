ba725e34ac69fa40f4fdfb693a734088e9ffc136
Cmsworks <yusupovb@gmail.com>
2012-10-29 14:20:37 +0400

    Add other folders

---
+++ datas/tmp/index.php
@@ -0,0 +1,14 @@
+<?PHP
+/**
+ * Directory blocker
+ *
+ * @package Cotonti
+ * @version 0.0.3
+ * @author Neocrome, Cotonti Team
+ * @copyright Copyright (c) Cotonti Team 2008-2009
+ * @license BSD
+ */
+
+/* ==== Directory blocker ==== */
+
+?>
